cee1d3f591408755be9bc6e7294e3d4299a55505
Raulyoyo <raul.dmai@gmail.com>
2023-04-25 16:59:49 -0600

    breadcrum corregido

--- src/app/libros/libros.component.ts
+++ src/app/libros/libros.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { SidebarService } from '../shared/sidebar/sidebar.service';
+import { ActivatedRoute } from '@angular/router'; // Importar
 
 @Component({
   selector: 'app-libros',
@@ -7,16 +8,39 @@ import { SidebarService } from '../shared/sidebar/sidebar.service';
   styleUrls: ['./libros.component.scss']
 })
 export class LibrosComponent {
-  constructor(public sidebarservice: SidebarService) { }
+  ruta : string;
+  bread : any = [];
+  constructor(public sidebarservice: SidebarService,  private route: ActivatedRoute) { }
   ngOnInit(): void {
+
+    
   }
   toggleSidebar() {
     this.sidebarservice.setSidebarState(!this.sidebarservice.getSidebarState());
+    
   }
   toggleBackgroundImage() {
     this.sidebarservice.hasBackgroundImage = !this.sidebarservice.hasBackgroundImage;
   }
   getSideBarState() {
+    this.route.children[0].url.subscribe(res => {
+      this.ruta = res[0].path;
+    });
+    this.ruta
+    switch(this.ruta){
+      case'Aluprelit': this.bread = {perfil:'Alumno', nivel: 'Preescolar', genero:'Literario'}; break;
+      case'Alupreinf': this.bread = {perfil:'Alumno', nivel: 'Preescolar', genero:'Informativo'}; break;
+      case'Aluprilit': this.bread = {perfil:'Alumno', nivel: 'Primaria', genero:'Literario'}; break;
+      case'Alupriinf': this.bread = {perfil:'Alumno', nivel: 'Primaria', genero:'Informativo'}; break;
+      case'Aluseclit': this.bread = {perfil:'Alumno', nivel: 'Secundaria', genero:'Literario'}; break;
+      case'Alusecinf': this.bread = {perfil:'Alumno', nivel: 'Secundaria', genero:'Informativo'}; break;
+      case'Famlit': this.bread = {perfil:'Padre de Familia', nivel: '', genero:'Literario'}; break;
+      case'Faminf': this.bread = {perfil:'Padre de Familia', nivel: '', genero:'Informativo'}; break;
+      case'Doclit': this.bread = {perfil:'Docente', nivel: '', genero:'Literario'}; break;
+      case'Docinf': this.bread = {perfil:'Docente', nivel: '', genero:'Informativo'}; break;
+      
+    }
+    //console.log(this.bread);
     return this.sidebarservice.getSidebarState();
   }
 
